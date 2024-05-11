1cb152d6caed696425a3f2329ce27558744217aa
Isaac <47983542+forbiddensandwich@users.noreply.github.com>
2024-05-11 14:38:20 -0400

    Delete FireEmblemCharacterTracker-main directory

--- FireEmblemCharacterTracker-main/FireEmblemCharacterTracker-main/client/src/FireEmblemData/Weapons.js
+++
@@ -1,6 +0,0 @@
-//FellStone FellRuinstone FellMagicstone FellWeightstone FellSpark FellSparkFX5, FellArcana
-//RuneSword AegisShield Thyrsus RafailGem FireBreath FogBreath IceBreath FlameBreath DarkBreath CamillasAxe Areadbhar SwordofCreator, Vajra-Mushti
-//Effectiveness
-//ForcedFollowUp Smash NoFollow
-//Range
-//Magic Physical
